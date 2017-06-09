b6a984bf9d13c15233ccee1e34225d5fdb494f44
s.lotishko <s.lotishko@aimprosoft.com>
2017-06-09 17:36:56 +0300

    SpringMVC

---
+++ back-end/src/main/java/com/myback/controller/SpringBootWebApplication.java
@@ -0,0 +1,24 @@
+package com.myback.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+/**
+ * Created by sveta on 09.06.17.
+ */
+
+@SpringBootApplication
+public class SpringBootWebApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootWebApplication.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SpringBootWebApplication.class, args);
+    }
+
+}
