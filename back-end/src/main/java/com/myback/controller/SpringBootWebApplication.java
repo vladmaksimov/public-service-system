802cbd29ca6da2ef5c1eb971c3a272494797d98d
s.lotishko <s.lotishko@aimprosoft.com>
2017-06-09 19:19:51 +0300

    SpringBoot

--- back-end/src/main/java/com/myback/controller/SpringBootWebApplication.java
+++ back-end/src/main/java/com/myback/controller/SpringBootWebApplication.java
@@ -3,7 +3,7 @@ package com.myback.controller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * Created by sveta on 09.06.17.
--- back-end/src/main/java/com/myback/controller/WelcomeController.java
+++ back-end/src/main/java/com/myback/controller/WelcomeController.java
@@ -1,12 +1,12 @@
 package com.myback.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
-@Controller
+@RestController
 public class WelcomeController {
 
     // inject via application.properties
